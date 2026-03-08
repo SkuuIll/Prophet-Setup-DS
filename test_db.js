ae3c30ddf9e484d6034f42a744553958a953d87e
Skuuill <moreappmix@gmail.com>
2026-03-08 16:20:00 +0000

    chore: remove various outdated test scripts.

--- test_db.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const Database = require('better-sqlite3');
-const db = new Database('data/prophet.sqlite');
-db.pragma('journal_mode = WAL');
-console.log('SQLite works');
--- test_seasons.js
+++
@@ -1,2 +0,0 @@
-const { getSeasons } = require('./modules/pubgApi');
-getSeasons('steam').then(s => console.log(s.map(x => x.id).slice(-20))).catch(console.error);
